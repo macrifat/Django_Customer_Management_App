b6fbf5d098e422c5cda7de4f5ad4546688a54a78
mac-rifat <macrifat@gmail.com>
2022-11-25 17:06:41 +0100

    dynamic order, customer, total order

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'myapp/dashboard.html')
+    orders = Order.objects.all()
+    customer = Customer.objects.all()
+    total_customer = customer.count()
+    total_order = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    diction = {'orders': orders, 
+               'customers': customer, 
+               'total_customer':total_customer,
+               'total_order': total_order,
+               'delivered': delivered,
+               'pending': pending
+               }
+    return render(request, 'myapp/dashboard.html', context=diction)
 
 def product(request):
-    return render(request, 'myapp/product.html')
+    product= Product.objects.all()
+    diction = {'products':product}
+    return render(request, 'myapp/product.html',context= diction)
 
 def customer(request):
     return render(request, 'myapp/customer.html')
