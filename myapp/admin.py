fc63836ccf5be693cc6219ab7ca39a42495d71f8
mac-rifat <macrifat@gmail.com>
2022-11-25 15:10:57 +0100

    add Product Model

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from myapp.models import Customer
+from myapp.models import Customer,Product
 
 # Register your models here.
 
 admin.site.register(Customer)
+admin.site.register(Product)
--- myapp/models.py
+++ myapp/models.py
@@ -7,3 +7,16 @@ class Customer(models.Model):
     phone = models.CharField(max_length=20, null = True)
     email = models.CharField(max_length=30, null = True)
     date_created = models.DateTimeField(auto_now_add=True, null = True)
+
+
+    def __str__(self):
+      return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50, null = True)
+    price = models.FloatField(null = True)
+    category = models.CharField(max_length=50, null = True)
+    description =models.CharField(max_length=50, null = True)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
+
